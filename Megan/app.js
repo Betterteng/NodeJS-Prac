f02cb41da485426ddbaf36ddd03071041344ef50
Betterteng <betterteng@gmail.com>
2017-08-23 12:29:07 +1000

    prac

--- Megan/app.js
+++ Megan/app.js
@@ -20,7 +20,7 @@ app.locals.videoData = require('./videodata.json');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing your favic on in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- Megan/routes/index.js
+++ Megan/routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// 其实也可以在js文件中传各种东西，这样如果不是global的话就会更有效了。
+var vd = require('../videodata.json');
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
         title: 'Home',
-        name: 'Oscar'
+        name: 'Oscar',
+        videoData2: vd
     });
 });
 
